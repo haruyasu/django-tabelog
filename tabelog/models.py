cf683d17e11f3e8b7db4b8b265fe3ef66218c12c
haruyasu <harukun2002@gmail.com>
2020-03-01 16:37:15 +0900

    カルーセル追加

--- tabelog/models.py
+++ tabelog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+  category_l = models.CharField("業態カテゴリ", max_length=10, blank=False)
+  name = models.CharField("業態名", max_length=30, blank=False)
+
+  def __str__(self):
+    return str(self.name)
+
+class Pref(models.Model):
+  pref = models.CharField("都道府県コード", max_length=6, blank=False)
+  name = models.CharField("都道府県名", max_length=10, blank=False)
+
+  def __str__(self):
+    return str(self.name)
--- tabelog/views.py
+++ tabelog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+
+class IndexView(TemplateView):
+  template_name = 'tabelog/index.html'
+
