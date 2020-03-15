fa72c1717d0c36d044b25eca7e89c59787d43be4
haruyasu <harukun2002@gmail.com>
2020-03-15 18:29:52 +0900

    :rocket: アカウント作成準備

--- tabelog/urls.py
+++ tabelog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('search', views.Search, name='search'),
     path('shop_info/<str:restid>', views.ShopInfo, name='shop_info'),
-    path('signup/', views.SignUp.as_view(), name='signup'),
-    path('login/', views.Login.as_view(), name='login'),
-    path('logout/', views.Logout.as_view(), name='logout'),
+    # path('signup/', views.SignUp.as_view(), name='signup'),
+    # path('login/', views.Login.as_view(), name='login'),
+    # path('logout/', views.Logout.as_view(), name='logout'),
 ]
