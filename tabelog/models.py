9edfe0263e3d018eff24ac591393c13f058e3176
haruyasu <harukun2002@gmail.com>
2020-03-20 12:03:55 +0900

    :zap: 微修正

--- tabelog/models.py
+++ tabelog/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    category_l = models.CharField("業態カテゴリ", max_length=10, blank=False)
-    name = models.CharField("業態名", max_length=30, blank=False)
+    category_l = models.CharField("大業態コード", max_length=10, blank=False)
+    name = models.CharField("カテゴリ名", max_length=30, blank=False)
 
     def __str__(self):
         return str(self.name)
@@ -18,16 +18,15 @@ class Pref(models.Model):
         return str(self.name)
 
 
-SCORE_CHOICES = [
-    (1, '★'),
-    (2, '★★'),
-    (3, '★★★'),
-    (4, '★★★★'),
-    (5, '★★★★★'),
-]
-
-
 class Review(models.Model):
+    SCORE_CHOICES = [
+        (1, '★'),
+        (2, '★★'),
+        (3, '★★★'),
+        (4, '★★★★'),
+        (5, '★★★★★'),
+    ]
+
     shop_id = models.CharField('店舗ID', max_length=10, blank=False)
     shop_name = models.CharField('店舗名', max_length=200, blank=False)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -36,12 +35,12 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_percent(self):
+        percent = round(self.score / 5 * 100)
+        return percent
+
     class Meta:
         unique_together = ('shop_id', 'user')
 
     def __str__(self):
         return str(self.shop_id)
-
-    def get_percent(self):
-        percent = round(self.score / 5 * 100)
-        return percent
